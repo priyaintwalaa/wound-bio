30be85ca8bfd7c8d650a24b236d16caf31f83cd4
priyaintwalaa <priya.intwala@bacancy.com>
2024-04-23 23:19:59 +0530

    test cases of user

--- functions/src/__tests__/users.test.ts
+++ functions/src/__tests__/users.test.ts
@@ -1,77 +1,74 @@
-// import supertest from 'supertest';
-import app from '../app.js';
-
 import request from 'supertest';
-// import app from '../app';
 
-describe('User Routes', () => {
+const BASE_URL = 'http://127.0.0.1:5001/woundbio/us-central1/wb';
+describe('User Route',()=>{
   describe('POST /api/users/system-admin', () => {
-    it('should create a system admin user with valid data', async () => {
-      const userData = {
-        email: 'test@example.com',
-        firstname: 'John',
-        lastname: 'Doe',
+    it('should create a new system user and return with role', async () => {
+      const newUser = {
+        firstname: 'Priya1111',
+        lastname: 'Intwala2222',
+        email: 'priyaintwala3333@bacancy.com',
       };
-
-      const res = await request(app)
+  
+      const response = await request(BASE_URL)
         .post('/api/users/system-admin')
         .set('x-api-key', process.env.SYSTEM_ADMIN_KEY)
-        .send(userData)
+        .send(newUser)
         .expect(200);
-
-      expect(res.body.success).toBe(true);
-      expect(res.body.data).toHaveProperty('id');
-      expect(res.body.message).toBe('User created successfully');
-    });
-
+  
+      expect(response.body).toHaveProperty('success', true);
+      expect(response.body).toHaveProperty('message', 'User created succesfully');
+      expect(response.body.data).toHaveProperty('role');
+    }, 20000);
+  
     it('should return 401 Unauthorized with invalid x-api-key', async () => {
       const userData = {
-        email: 'test@example.com',
+        email: 'mydata0704@gmail.com',
         firstname: 'John',
         lastname: 'Doe',
       };
-
-      const res = await request(app)
+  
+      const res = await request(BASE_URL)
         .post('/api/users/system-admin')
         .set('x-api-key', 'invalid-api-key')
         .send(userData)
         .expect(401);
-
+  
       expect(res.body.error).toBe('Unauthorized');
-    });
-
+    }, 20000);
+  
+  
     it('should return 400 Bad Request with invalid email', async () => {
       const userData = {
         email: 'invalid-email',
         firstname: 'John',
         lastname: 'Doe',
       };
-
-      const res = await request(app)
+  
+      const res = await request(BASE_URL)
         .post('/api/users/system-admin')
         .set('x-api-key', process.env.SYSTEM_ADMIN_KEY)
         .send(userData)
         .expect(400);
-
+  
       expect(res.body.error).toBe('Invalid data');
       expect(res.body.details).toContainEqual({
         message: 'email is Invalid email',
       });
     });
-
+  
     it('should return 400 Bad Request with missing fields', async () => {
       const userData = {
-        email: 'test@example.com',
+        email: 'mydata0704@gmail.com',
       };
-
-      const res = await request(app)
+  
+      const res = await request(BASE_URL)
         .post('/api/users/system-admin')
         .set('x-api-key', process.env.SYSTEM_ADMIN_KEY)
         .send(userData)
         .expect(400);
-
-      expect(res.body.error).toBe('Invalid data');
-      // expect(res.body.details).toContainEqual([{"message": "firstname is Required"}, {"message": "lastname is Required"}]);
+  
+      expect(res.body.error).toBe('Invalid data')
       expect(res.body.details).toContainEqual(
         expect.objectContaining({ message: 'firstname is Required' })
       );
@@ -79,112 +76,71 @@ describe('User Routes', () => {
         expect.objectContaining({ message: 'lastname is Required' })
       );
     });
+  
+    const newUser = {
+      firstname: 'Priy',
+      lastname: 'Intwal',
+      email: 'priyaintwal@bacancy.com',
+    }
+    let userId: string 
+    describe('POST /api/users', () => {
+      it('should create a new user', async () => {
+        const response = await request(BASE_URL)
+          .post('/api/users')
+          .send(newUser)
+          .expect(200);
+  
+        expect(response.body).toHaveProperty('success', true);
+        expect(response.body).toHaveProperty('message', 'User created successfully');
+        expect(response.body.data).toHaveProperty('id');
+        userId = response.body.data.id;
+      });
+    });
+  
+    describe('GET /api/users/:id', () => {
+      it('should get a user by ID', async () => {
+        const response = await request(BASE_URL)
+          .get(`/api/users/${userId}`)
+          .expect(200);
+  
+        expect(response.body).toHaveProperty('success', true);
+        expect(response.body.data).toHaveProperty('firstname', newUser.firstname);
+        expect(response.body.data).toHaveProperty('lastname', newUser.lastname);
+        expect(response.body.data).toHaveProperty('email', newUser.email);
+      });
+    });
+  
+    describe('PUT /api/users/:id', () => {
+      it('should update a user', async () => {
+        const updatedUser = {
+          firstname: 'Jane',
+          lastname: 'Smith',
+          email: 'janesmith@example.com',
+        };
+  
+        const response = await request(BASE_URL)
+          .put(`/api/users/${userId}`)
+          .send(updatedUser)
+          .expect(200);
+  
+        expect(response.body).toHaveProperty('success', true);
+        expect(response.body).toHaveProperty('message', 'User updated succesfully');
+        expect(response.body.data).toHaveProperty('firstname', updatedUser.firstname);
+        expect(response.body.data).toHaveProperty('lastname', updatedUser.lastname);
+        expect(response.body.data).toHaveProperty('email', updatedUser.email);
+      });
+    });
+  
+    describe('DELETE /api/users/:id', () => {
+      it('should delete a user', async () => {
+        const response = await request(BASE_URL)
+          .delete(`/api/users/${userId}`)
+          .expect(200);
+  
+        expect(response.body).toHaveProperty('success', true);
+        expect(response.body).toHaveProperty('message', 'User deleted succesfully');
+      });
+    });
   });
-});
-// describe('User', () => {
-//   it('should create a new user', async () => {
-//     const userData = {
-//       firstname: 'Test',
-//       lastname: 'User',
-//       email: 'test@example.com',
-//     };
+})
 
-//     const res = await supertest(app)
-//       .post('/api/users/system-admin')
-//       .send(userData)
-//       .set('x-api-key', process.env.SYSTEM_ADMIN_KEY)
-//       .expect(200);
-//   });
-// });
-// describe('User',()=>{
-//   it('should create a new user',async (done)=>{
-//     const userData = {
-//       firstname: 'Test',
-//       lastname: 'User',
-//       email: 'test@example.com',
-//     };
-//     supertest('/api/users')
-//       .post('/system-admin')
-//       .send(userData)
-//       .set('x-api-key', process.env.SYSTEM_ADMIN_KEY)
-//       .end(function(err,res){
-//         expect(res.statusCode).toBe(200)
-//         if(err){
-//           throw err
-//         }
-//         done()
-//       })
-//   })
-// })
-
- 
-    
-// const res = await supertest(app).post('/api/users/system-admin')
-// .send(userData)
-// .set('x-api-key', process.env.SYSTEM_ADMIN_KEY)
-// .expect(200);
-
-// expect(res)
-
-
-
-
-// describe('User Routes', () => {
-//   describe('POST /system-admin', () => {
-//     it('should create a system admin user', async () => {
-//       const userData = {
-//         username: 'testuser',
-//         firstname: 'Test',
-//         lastname: 'User',
-//         email: 'test@example.com',
-//         role: 'SYSTEM_ADMIN',
-//         roleId: '',
-//         companyId: '',
-//       };
-
-//       const res = await supertest(app).post('/api/users/system-admin')
-//         // .post('/system-admin')
-//         .set('x-api-key', process.env.SYSTEM_ADMIN_KEY)
-//         .send(userData)
-//         .expect(200);
-
-//       expect(res.body.success).toBe(true);
-//       expect(res.body.data).toHaveProperty('id');
-//       expect(res.body.message).toBe('User created successfully');
-//     });
-
-//     it('should return 401 if invalid x-api-key is provided', async () => {
-//       const userData = {
-//         username: 'testuser',
-//         firstname: 'Test',
-//         lastname: 'User',
-//         email: 'test@example.com',
-//         role: 'SYSTEM_ADMIN',
-//         roleId: '',
-//         companyId: '',
-//       };
-
-//       const res = await supertest(app)
-//         .post('/system-admin')
-//         .set('x-api-key', 'invalid-api-key')
-//         .send(userData)
-//         .expect(401);
-
-//       expect(res.body.success).toBe(false);
-//       expect(res.body.message).toBe('Unauthorized');
-//     });
-//   });
-// });
-
-// const sum = require('./sum');
-// import {sum} from '../controllers/sum.js'
-
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
-
-describe('Example test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-});
