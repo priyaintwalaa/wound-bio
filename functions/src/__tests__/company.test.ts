574656cd7a6c4cce84388cd938ace0510c70fe7b
priyaintwalaa <priya.intwala@bacancy.com>
2024-04-26 11:52:24 +0530

    import token in manufacturer.test

--- functions/src/__tests__/company.test.ts
+++ functions/src/__tests__/company.test.ts
@@ -1,144 +1,144 @@
-// import request from 'supertest';
-// const BASE_URL = 'http://127.0.0.1:5001/fir-functions-9c002/us-central1/wb';
+import request from 'supertest';
+const BASE_URL = 'http://127.0.0.1:5001/fir-functions-9c002/us-central1/wb';
 
-// let token: string;
-// let companyId: string;
-// let userId: string;
-// describe('company', () => {
-//   it('return token for a valid user and create company', async () => {
-//     const companyName = {
-//       name: "hello"
-//     }
+export let token: string;
+let companyId: string;
+let userId: string;
+describe('company', () => {
+  it('return token for a valid user and create company', async () => {
+    const companyName = {
+      name: "hello"
+    }
 
-//     const loginData = {
-//       email: "hello@bacancy.com",
-//       password: "af12f4d30f"
-//     }
+    const loginData = {
+      email: "johndoe@example.com",
+      password: "89b1c5e046"
+    }
 
-//     const res = await request(BASE_URL)
-//       .post('/api/auth/login')
-//       .send(loginData)
-//       .expect(200)
+    const res = await request(BASE_URL)
+      .post('/api/auth/login')
+      .send(loginData)
+      .expect(200)
 
-//     expect(res.body).toHaveProperty('success', true)
-//     token = res.body.data.token
-//     userId = res.body.data.user.id
+    expect(res.body).toHaveProperty('success', true)
+    token = res.body.data.token
+    userId = res.body.data.user.id
 
-//     const companyData = await request(BASE_URL)
-//       .post('/api/companies')
-//       .set('Authorization', `Bearer ${token}`)
-//       .send(companyName)
-//       .expect(200)
+    const companyData = await request(BASE_URL)
+      .post('/api/companies')
+      .set('Authorization', `Bearer ${token}`)
+      .send(companyName)
+      .expect(200)
 
-//     expect(companyData.body).toHaveProperty('success', true)
-//     expect(companyData.body.data).toHaveProperty('id')
-//     companyId = companyData.body.data.id
+    expect(companyData.body).toHaveProperty('success', true)
+    expect(companyData.body.data).toHaveProperty('id')
+    companyId = companyData.body.data.id
    
-//   }, 20000)
+  }, 20000)
 
-//   it('should update a company', async () => {
-//     const updatedCompany = {
-//       name: 'Updated Test Company',
-//     };
+  it('should update a company', async () => {
+    const updatedCompany = {
+      name: 'Updated Test Company 1',
+    };
 
-//     const response = await request(BASE_URL)
-//       .put(`/api/companies/${companyId}`)
-//       .set('Authorization', `Bearer ${token}`)
-//       .send(updatedCompany)
-//       .expect(200);
+    const response = await request(BASE_URL)
+      .put(`/api/companies/${companyId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(updatedCompany)
+      .expect(200);
 
-//     expect(response.body).toHaveProperty('success', true);
-//   });
+    expect(response.body).toHaveProperty('success', true);
+  });
 
 
-//   it('should delete a company', async () => {
-//     const response = await request(BASE_URL)
-//       .delete(`/api/companies/${companyId}`)
-//       .set('Authorization', `Bearer ${token}`)
-//       .expect(200);
+  it('should delete a company', async () => {
+    const response = await request(BASE_URL)
+      .delete(`/api/companies/${companyId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
 
-//     expect(response.body).toHaveProperty('success', true);
-//   });
+    expect(response.body).toHaveProperty('success', true);
+  });
 
    
 
-//   it('should create a new company user POST /api/companies/:companyId/users', async () => {
-//     const companyName = {
-//       name: "yupppiiii"
-//     }
-//     const companyData = await request(BASE_URL)
-//       .post('/api/companies')
-//       .set('Authorization', `Bearer ${token}`)
-//       .send(companyName)
-//       .expect(200)
+  it('should create a new company user POST /api/companies/:companyId/users', async () => {
+    const companyName = {
+      name: "yupppffiiii"
+    }
+    const companyData = await request(BASE_URL)
+      .post('/api/companies')
+      .set('Authorization', `Bearer ${token}`)
+      .send(companyName)
+      .expect(200)
 
-//     expect(companyData.body.data).toHaveProperty('id')
-//     companyId = companyData.body.data.id
+    expect(companyData.body.data).toHaveProperty('id')
+    companyId = companyData.body.data.id
 
-//     const userUpdate = await request(BASE_URL)
-//     .put(`/api/users/${userId}`)
-//     .set('Authorization', `Bearer ${token}`)
-//     .send({companyId})
-//     .expect(200)
+    const userUpdate = await request(BASE_URL)
+    .put(`/api/users/${userId}`)
+    .set('Authorization', `Bearer ${token}`)
+    .send({companyId})
+    .expect(200)
 
-//     expect(userUpdate.body).toHaveProperty('success', true);
+    expect(userUpdate.body).toHaveProperty('success', true);
 
-//     const newUser = {
-//       firstname: 'John',
-//       lastname: 'Doe',
-//       email: 'doeeJohnn@example.com',
-//       companyId,
-//       role:'companyadmin'
-//     };
+    const newUser = {
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'great@example.com',
+      companyId,
+      role:'companyadmin'
+    };
 
-//     const response = await request(BASE_URL)
-//       .post(`/api/companies/${companyId}/users`)
-//       .set('Authorization', `Bearer ${token}`)
-//       .send(newUser)
-//       .expect(200);
+    const response = await request(BASE_URL)
+      .post(`/api/companies/${companyId}/users`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(newUser)
+      .expect(200);
 
-//     expect(response.body).toHaveProperty('success', true);
-//     expect(response.body.data.user).toHaveProperty('id');
-//     userId = response.body.data.user.id;
-//   });
+    expect(response.body).toHaveProperty('success', true);
+    expect(response.body.data.user).toHaveProperty('id');
+    userId = response.body.data.user.id;
+  });
 
-//   it('should get a company by ID', async () => {
-//     const response = await request(BASE_URL)
-//       .get(`/api/companies/${companyId}`)
-//       .set('Authorization', `Bearer ${token}`)
-//       .expect(200);
+  it('should get a company by ID', async () => {
+    const response = await request(BASE_URL)
+      .get(`/api/companies/${companyId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
 
-//     expect(response.body).toHaveProperty('success', true);
-//   });
-
-
-//   it('should update a company user /api/companies/:companyId/users/:userId', async () => {
-//     const updatedUser = {
-//       firstname: 'Jane',
-//       lastname: 'Smith',
-//       email: 'SmiithennJanee@example.com',
-//     };
-
-//     const response = await request(BASE_URL)
-//       .put(`/api/companies/${companyId}/users/${userId}`)
-//       .set('Authorization', `Bearer ${token}`)
-//       .send(updatedUser)
-//       .expect(200);
-
-//     expect(response.body).toHaveProperty('success', true);
-//   });
+    expect(response.body).toHaveProperty('success', true);
+  });
 
 
-//   it('should delete a company user DELETE /api/companies/:companyId/users/:userId', async () => {
-//     const response = await request(BASE_URL)
-//       .delete(`/api/companies/${companyId}/users/${userId}`)
-//       .set('Authorization', `Bearer ${token}`)
-//       .expect(200);
+  it('should update a company user /api/companies/:companyId/users/:userId', async () => {
+    const updatedUser = {
+      firstname: 'Jane',
+      lastname: 'Smith',
+      email: 'SmitgreatJanee@example.com',
+    };
 
-//     expect(response.body).toHaveProperty('success', true);
-//   });
+    const response = await request(BASE_URL)
+      .put(`/api/companies/${companyId}/users/${userId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(updatedUser)
+      .expect(200);
 
-// });
+    expect(response.body).toHaveProperty('success', true);
+  });
+
+
+  it('should delete a company user DELETE /api/companies/:companyId/users/:userId', async () => {
+    const response = await request(BASE_URL)
+      .delete(`/api/companies/${companyId}/users/${userId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+
+    expect(response.body).toHaveProperty('success', true);
+  });
+
+});
 
 describe('Example test', () => {
   it('should pass', () => {
--- functions/src/__tests__/manufacturer-product.test.ts
+++ functions/src/__tests__/manufacturer-product.test.ts
@@ -1,27 +1,15 @@
 import request from 'supertest';
+import {token as UserToken} from './company.test.js'
 const BASE_URL = 'http://127.0.0.1:5001/fir-functions-9c002/us-central1/wb';
 
-let token: string;
 let manufacturerId: string
 let productId: string;
 describe('Manufacturer and Product Routes', () => {
     it('it should create a new manufacturer', async () => {
-        const loginData = {
-            email: "hello@bacancy.com",
-            password: "af12f4d30f"
-        }
-
-        const res = await request(BASE_URL)
-            .post('/api/auth/login')
-            .send(loginData)
-            .expect(200)
-
-        expect(res.body).toHaveProperty('success', true)
-        token = res.body.data.token
 
         const manufacturerCreate = await request(BASE_URL)
             .post('/api/manufacturers')
-            .set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${UserToken}`)
             .send({ name: "cetemol" })
             .expect(200)
 
@@ -33,7 +21,7 @@ describe('Manufacturer and Product Routes', () => {
     it('update the manufacturer', async () => {
         const manufacturerUpdate = await request(BASE_URL)
             .put(`/api/manufacturers/${manufacturerId}`)
-            .set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${UserToken}`)
             .send({ name: "paracetemol" })
             .expect(200)
 
@@ -43,7 +31,7 @@ describe('Manufacturer and Product Routes', () => {
     it('it should delete the manufacturer', async () => {
         const manufacturerDelete = await request(BASE_URL)
             .delete(`/api/manufacturers/${manufacturerId}`)
-            .set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${UserToken}`)
             .expect(200)
 
 
@@ -53,7 +41,7 @@ describe('Manufacturer and Product Routes', () => {
     it('it should create product of the manufacturer', async () => {
         const manufacturerCreate = await request(BASE_URL)
             .post('/api/manufacturers')
-            .set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${UserToken}`)
             .send({ name: "cetemol" })
             .expect(200)
 
@@ -63,7 +51,7 @@ describe('Manufacturer and Product Routes', () => {
 
         const productCreate = await request(BASE_URL)
             .post(`/api/manufacturers/${manufacturerId}/product`)
-            .set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${UserToken}`)
             .send(productData)
             .expect(200)
 
@@ -78,7 +66,7 @@ describe('Manufacturer and Product Routes', () => {
 
         const productCreate = await request(BASE_URL)
             .put(`/api/manufacturers/${manufacturerId}/product/${productId}`)
-            .set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${UserToken}`)
             .send(productData)
             .expect(200)
 
@@ -88,9 +76,15 @@ describe('Manufacturer and Product Routes', () => {
     it('it should delete the product', async () => {
         const productDelete = await request(BASE_URL)
             .delete(`/api/manufacturers/${manufacturerId}/product/${productId}`)
-            .set('Authorization', `Bearer ${token}`)
+            .set('Authorization', `Bearer ${UserToken}`)
             .expect(200)
 
         expect(productDelete.body).toHaveProperty('success', true)
     })
 })
+
+describe('Example test', () => {
+    it('should pass', () => {
+      expect(true).toBe(true);
+    });
+  });
